3798a32cdc670f94abb8e2617db6ec856d6802b4
Gouthamsai15 <gouthamsai005@gmail.com>
2026-09-03 21:33:24 +0530

    Fix Vercel deployment upload middleware

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const adminController = require('../controllers/adminController');
-const upload = require('../middleware/uploadMiddleware');
 
 // Admin Dashboard
 router.get('/', adminController.dashboard_get);
